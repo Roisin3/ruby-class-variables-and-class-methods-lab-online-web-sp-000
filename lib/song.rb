7acf658a5ccd61f8d1d4b66c09ad5b68a83c2c44
Michael Lynch <michael@michaelrlynch.com>
2019-08-30 23:26:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,12 @@
 class Song
   attr_accessor :name, :artist, :genre
   
-  @@song_count = 0
+  #@@song_count = 0
   
-  def initialize
-    @@song_count += 1
-  end
   
-  def self.count
-    @@song_count
-  end
+  #def self.count
+  #  @@song_count
+  #end
   
   def new(name, artist, genre)
   
