ad0a5eb8889ef3b6b06f94f00aec5435c71bc927
Michael Lynch <michael@michaelrlynch.com>
2019-08-31 21:13:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,45 +1,28 @@
 class Song
   attr_accessor :name, :artist, :genre
-  
+
   @@count = 0
   @@artists = []
   @@genres = []
-  
-  def initalize(name, artist, genre)
+
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count += 1
+    @@count +=1
     @@genres << genre
     @@artists << artist
   end
-  
+
   def self.count
     @@count
   end
-  
+
   def self.artists
     @@artists.uniq
   end
-  
-  def genres
-    @@genres.uniq
-  end
-  
-  def genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-    
-  end
-  
-  def artist_count
+
+  def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
@@ -49,8 +32,21 @@ class Song
       end
     end
     artist_count
-    
   end
-    
-  
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 end
