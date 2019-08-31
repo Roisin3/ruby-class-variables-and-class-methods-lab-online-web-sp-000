eb397553e3a5961b36aa079722f7d2305a8e9d45
Michael Lynch <michael@michaelrlynch.com>
2019-08-31 21:11:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,13 @@ class Song
   end
   
   def artist_count
-    
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
   end
     
   
