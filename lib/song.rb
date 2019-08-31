60a33500d265d4717b10d975e315c7637def427d
Michael Lynch <michael@michaelrlynch.com>
2019-08-31 21:11:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,15 @@ class Song
   end
   
   def genre_count
-    
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
     
   end
   
@@ -39,6 +47,9 @@ class Song
       else
         artist_count[artist] = 1
       end
+    end
+    artist_count
+    
   end
     
   
