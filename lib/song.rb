51cafa55d73be41adc05eacfa86539dbd47cb42f
Michael Lynch <michael@michaelrlynch.com>
2019-08-31 00:17:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,16 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
-  @@artist = []
-  @@genre = []
+  @@artists = []
+  @@genres = []
   
   def initalize(name, artist, genre)
     @@name = name
-    @@artist = artist
-    @@genre = genre
+    @@artists = artist
+    @@genres = genre
     @@count += 1
-    @@genre << genre
-    @@artist << artist
+    @@genres << genre
+    @@artists << artist
   end
   
   
